4d120019007ddf2d4216d4d48656dd97f4b845e2
Mike Boeser <mboeser@yahoo.com>
2016-02-01 01:11:47 -0600

    add vote once logic

--- server/public/scripts/app.js
+++ server/public/scripts/app.js
@@ -7,11 +7,14 @@ myApp.controller('myCtrl', ['$scope', '$http', '$filter', function ($scope, $htt
     $scope.video = {};
     $scope.view = {};
     $scope.vote = {};
+    $scope.votes = [];
     $scope.videos = [];
     $scope.checked = false;
     $scope.limitNumber = null;
     $scope.isActive = 0;
     $scope.isWeekBtnNum = 0;
+    $scope.today = new Date(Date.now());
+
 
     $scope.activeButton = function(num) {
         $scope.isActive = num;
@@ -41,7 +44,7 @@ myApp.controller('myCtrl', ['$scope', '$http', '$filter', function ($scope, $htt
 
         var today = new Date(Date.now());
 
-        return /[12345]/.test(today.getDay());
+        return /[06]/.test(today.getDay());
     };
 
 
@@ -103,12 +106,26 @@ myApp.controller('myCtrl', ['$scope', '$http', '$filter', function ($scope, $htt
     $scope.voteOne = function (vid, num) {
         console.log(vid);
 
+        var vidVote = {};
+
         $scope.vote = {
             opinion: num
         };
-        $http.post('https://proofapi.herokuapp.com/videos/' + vid.id + '/votes', $scope.vote, {headers: {'X-Auth-Token': 'ZU2nsMBQqKnvEwPbKsczgJEv'}}).then(function (response) {
-            console.log('new vote added', response);
-            $scope.getVideos();
+
+        $http.get('https://proofapi.herokuapp.com/videos/' + vid.id + '/votes', {headers: {'X-Auth-Token': 'ZU2nsMBQqKnvEwPbKsczgJEv'}}).then(function (response) {
+            vidVote = response.data.data;
+            console.log('here is the video info', vidVote);
+
+            if (vidVote.length === 0 || new Date(vidVote[0].attributes.created_at).getDay() != $scope.today.getDay()) {
+                $http.post('https://proofapi.herokuapp.com/videos/' + vid.id + '/votes', $scope.vote, {headers: {'X-Auth-Token': 'ZU2nsMBQqKnvEwPbKsczgJEv'}}).then(function (response) {
+                    console.log('new vote added', response);
+                    $scope.getVideos();
+                });
+            } else {
+                    console.log('dup vote');
+                    alert("Sorry, One Vote per Video per Day");
+                    vidVote = {};
+            }
         });
     };
 
@@ -121,6 +138,16 @@ myApp.controller('myCtrl', ['$scope', '$http', '$filter', function ($scope, $htt
         });
     };
 
+    //GET all the votes
+    $scope.getVotes = function () {
+        $http.get('https://proofapi.herokuapp.com/votes', {headers: {'X-Auth-Token': 'ZU2nsMBQqKnvEwPbKsczgJEv'}}).then(function (response) {
+            $scope.votes = response;
+            console.log($scope.votes);
+        });
+    };
+
+
     $scope.getVideos();
+    $scope.getVotes();
 
 }]);
